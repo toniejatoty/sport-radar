b84702cd3eb5ecacbded2abd11d9a160c07b8c5c
Konrad Wołkiewicz <konrad.wolkiewicz@o2.pl>
2026-03-26 21:11:20 +0100

    added groups view and add

---
+++ football-calendar/src/main/java/com/sportradar/football_calendar/controller/GroupsController.java
@@ -0,0 +1,37 @@
+package com.sportradar.football_calendar.controller;
+import com.sportradar.football_calendar.model.Group;
+import com.sportradar.football_calendar.repository.GroupRepository;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class GroupsController {
+
+    private final GroupRepository groupsRepository;
+    public GroupsController( GroupRepository groupsRepository) {
+        this.groupsRepository = groupsRepository;
+    }
+
+    @GetMapping("/groups") 
+    public String listTeams(Model model) {
+        model.addAttribute("group", groupsRepository.findAll());
+        return "Group"; 
+    }
+    
+    @GetMapping("/groups/add")
+    public String showAddForm(Model model) {
+    model.addAttribute("group", new Group()); 
+    return "add-group"; 
+}
+    @PostMapping("/groups/save")
+    public String saveTeam (@ModelAttribute("group") Group group) {
+    groupsRepository.save(group); 
+    return "redirect:/events"; 
+}
+
+
+}
--- football-calendar/src/main/java/com/sportradar/football_calendar/model/Group.java
+++ football-calendar/src/main/java/com/sportradar/football_calendar/model/Group.java
@@ -9,5 +9,21 @@ public class Group {
     @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Integer id;
     private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
             }
